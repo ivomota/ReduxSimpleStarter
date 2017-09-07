761985977e87723dfb6efd85962ebb208774c4cf
Ivo Mota <ivo.mota@farfetch.com>
2017-09-07 17:52:53 +0100

    Redux with container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Top cenas'},
+        {title: 'Porreiro'},
+        {title: 'Maias'},
+        {title: 'The javascript bad parts'}
+    ]
+}
