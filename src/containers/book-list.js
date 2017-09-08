c5ff8e1ce7e84eb5710e648a526cb8abf0bacd56
Ivo Mota <ivo.mota@farfetch.com>
2017-09-08 09:39:40 +0100

    Redux final version

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectBook } from '../actions/index'
 
 class Booklist extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li
+                key={book.title}
+                onClick={() => this.props.selectBook(book)}
+                className="list-group-item">
+                    {book.title}
+                </li>
             )
         })
     }
@@ -25,4 +33,8 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(Booklist);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Booklist);
